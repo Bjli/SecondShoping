e0a11cb209b99021d7310aac9ee28b81bcee256c
ljb <ljb_nwuer@163.com>
2017-04-26 22:59:59 +0800

    all(4.26)

---
+++ src/com/es/action/AbstractAction.java
@@ -0,0 +1,20 @@
+package com.es.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+@SuppressWarnings("serial")
+public abstract class AbstractAction extends ActionSupport implements ModelDriven, SessionAware {
+
+	protected Map<String, Object> session;
+	
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+	
+}
